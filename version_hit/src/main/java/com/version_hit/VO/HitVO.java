f06c2ef191d873e55abb9d104d374ba83230fdf2
GARZHI <191250009@smail.nju.edu.cn>
2021-11-06 19:41:15 +0800

    dev: Fill Hit VO/PO

--- version_hit/src/main/java/com/version_hit/VO/HitVO.java
+++ version_hit/src/main/java/com/version_hit/VO/HitVO.java
@@ -4,58 +4,16 @@ package com.version_hit.VO;
 import com.version_hit.PO.HitPO;
 
 public class HitVO {
-    private String download_url;
+    private Integer aid;
+    private String version;
+    private String platform;
+    private String device_id;
+    private String os_api;
+    private String channel;
+    private String version_code;
     private String update_version_code;
-    private String md5;
-    private String title;
-    private String update_tips;
+    private String cpu_arch;
 
     public HitVO(){}
-    public HitVO(HitPO hitPO){
-        this.download_url = hitPO.getDownload_url();
-        this.update_version_code = hitPO.getUpdate_version_code();
-        this.md5=hitPO.getMd5();
-        this.title=hitPO.getTitle();
-        this.update_tips=hitPO.getUpdate_tips();
-    }
 
-    public String getDownload_url() {
-        return download_url;
-    }
-
-    public void setDownload_url(String download_url) {
-        this.download_url = download_url;
-    }
-
-    public String getUpdate_version_code() {
-        return update_version_code;
-    }
-
-    public void setUpdate_version_code(String update_version_code) {
-        this.update_version_code = update_version_code;
-    }
-
-    public String getMd5() {
-        return md5;
-    }
-
-    public void setMd5(String md5) {
-        this.md5 = md5;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getUpdate_tips() {
-        return update_tips;
-    }
-
-    public void setUpdate_tips(String update_tips) {
-        this.update_tips = update_tips;
-    }
 }
